e916045f69fcfbfffc5328ab99b0dccaf032d128
Pranal4 <ramtekepranal78@gmail.com>
2026-08-24 21:34:24 +0530

    Fix 8-to-1 mux filename and test labels

--- Month1_Verilog/Week1_Combinational/tb_mux_8to1.v
+++ Month1_Verilog/Week1_Combinational/tb_mux_8to1.v
@@ -20,25 +20,25 @@ initial begin
     $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[0]);
 
     sel = 3'b001; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[1]);
+    $display("S=%b D=%b Y=%b (expect I1=%b)", sel, data, y, data[1]);
 
     sel = 3'b010; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[2]);
+    $display("S=%b D=%b Y=%b (expect I2=%b)", sel, data, y, data[2]);
 
     sel = 3'b011; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[3]);
+    $display("S=%b D=%b Y=%b (expect I3=%b)", sel, data, y, data[3]);
 
     sel = 3'b100; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[4]);
+    $display("S=%b D=%b Y=%b (expect I4=%b)", sel, data, y, data[4]);
 
     sel = 3'b101; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[5]);
+    $display("S=%b D=%b Y=%b (expect I5=%b)", sel, data, y, data[5]);
 
     sel = 3'b110; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[6]);
+    $display("S=%b D=%b Y=%b (expect I6=%b)", sel, data, y, data[6]);
 
     sel = 3'b111; #10;
-    $display("S=%b D=%b Y=%b (expect I0=%b)", sel, data, y, data[7]);
+    $display("S=%b D=%b Y=%b (expect I7=%b)", sel, data, y, data[7]);
     $finish;
 end
     
